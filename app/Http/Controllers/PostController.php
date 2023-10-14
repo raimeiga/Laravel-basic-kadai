482cd45df944245196bebeb38e7ae8091985ccac
raimeiga <yusuke612@hotmail.com>
2023-10-14 21:59:26 +0900

    提出用作成してみた

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;   
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller {
     public function index() {
-        return view('posts.index');  
-     }
+        // postsテーブルからすべてのデータを取得し、変数$postに代入する
+        $posts = DB::table('posts')->select('title','content')->get();
+ 
+        // 変数$postをposts/index.blade.phpファイルに渡す
+        return view('posts.index', compact('posts'));
+    }
 }
+
 
