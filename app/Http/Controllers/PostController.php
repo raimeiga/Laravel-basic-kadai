92dd08fd15868aee0748b9319ae6ea6da1cc934d
raimeiga <yusuke612@hotmail.com>
2023-10-15 11:13:44 +0900

    12行目 カラムのselectから、全てのカラム（全データ）の取得に修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\DB;
 class PostController extends Controller {
     public function index() {
         // postsテーブルからすべてのデータを取得し、変数$postに代入する
-        $posts = DB::table('posts')->select('title','content')->get();
+        $posts = DB::table('posts')->get();
  
         // 変数$postをposts/index.blade.phpファイルに渡す
         return view('posts.index', compact('posts'));
