83602efcd40cb4bd81c52d4e4d914010d61c5759
raimeiga <yusuke612@hotmail.com>
2023-10-19 12:49:55 +0900

    課題/データの作成機能とバリデーションを実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -23,5 +23,28 @@ class PostController extends Controller {
         // 変数$postをposts/show.blade.phpファイルに渡す
         return view('posts.show', compact('posts'));
     }  
+
+    public function create() {
+        return view('posts.create');
+    }
+    
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        // リダイレクトさせる   
+        return redirect('/posts');
+    }      
+    
+
 }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,11 +4,12 @@
  <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>投稿一覧</title>
+     
  </head>
  
  <body>
-     <table>
+     <h1>投稿一覧</h1>
+     <table>        
          <tr>
              <th>タイトル</th>
              <th>本文</th>             
