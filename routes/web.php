cddf16c99cdb97a9e59086ec9d108860b7022e23
raimeiga <yusuke612@hotmail.com>
2023-10-24 08:55:26 +0900

    ルーティングの復元

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
- <!--ルーティングファイル -->
- <?php  
+<?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\PostController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,6 +17,18 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/posts', [PostController::class, 'index']);
 
